0611533f2e6d1b5e88ae2170059e9ea797a77785
Taner105 <taneraydogan59@gmail.com>
2022-02-06 00:56:25 +0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () => {
+menu.addEventListener("click", () => {
     console.log("tıklandı");
-    navbar.classList.add("active");
-}
-window.onscroll = () => {
-    navbar.classList.remove("active");
-}
+    menu.classList.toggle("active");
+    navbar.classList.toggle("active")
+})
+// window.onscroll = () => {
+//     navbar.classList.remove("active");
+// }
+document.querySelectorAll(".navbar").forEach(n => n.addEventListener("click", () => {
+    menu.classList.remove("active");
+    navbar.classList.remove("active")
+}))
+
+
